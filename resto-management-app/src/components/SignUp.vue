0c7606001d0d4bbc3fa6f29e059f8be55fd0789f
Adkasy <adityakasyidi09@gmail.com>
2023-04-08 02:01:03 +0700

    feat: add project inizialitation

---
+++ resto-management-app/src/components/SignUp.vue
@@ -0,0 +1,36 @@
+<template>
+  <h3>Don't Have An Account? Register Here</h3>
+  <div class="register-form">
+    <input type="text" name="" id="" placeholder="Name" />
+    <input type="email" name="" id="" placeholder="Email" />
+    <input type="password" name="" id="" placeholder="Password" />
+    <button>Sign Up</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+};
+</script>
+
+<style>
+.register-form input {
+  height: 50px;
+  width: 330px;
+  padding-left: 20px;
+  border: 2px solid black;
+  border-radius: 10px;
+  display: block;
+  margin: 0 auto 10px;
+}
+
+button {
+  border-radius: 10px;
+  height: 35px;
+  width: 150px;
+  background-color: dimgrey;
+  color: white;
+  border: 2px solid black;
+}
+</style>
